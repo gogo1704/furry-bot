ce81c83d46c8f7b5f3bddb3d5ecfd99dd005b770
GogoFox <gogo-2001@o2.pl>
2020-11-11 00:44:06 +0100

    indentation error

--- commands/server.js
+++ commands/server.js
@@ -3,13 +3,13 @@ module.exports = {
     description: "Shows server informations.",
     execute(message, args) {
 		message.channel.send(`Server name: ${message.guild.name}
-		Server names acronym: ${message.guild.nameAcronym}
-		Total members: ${message.guild.memberCount}
-		Server creation data: ${message.guild.createdAt}
-		Server region: ${message.guild.region}
-		Server owner: ${message.guild.owner}
-		Channel available: ${message.guild.channels}
-		Emojis: ${message.guild.emojis}
-		My name: ${message.guild.me}`);
-  }
+Server names acronym: ${message.guild.nameAcronym}
+Total members: ${message.guild.memberCount}
+Server creation data: ${message.guild.createdAt}
+Server region: ${message.guild.region}
+Server owner: ${message.guild.owner}
+Channel available: ${message.guild.channels}
+Emojis: ${message.guild.emojis}
+My name: ${message.guild.me}`);
+	}		
 };
