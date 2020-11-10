650163799bd9b970d5daedd9a43420380343ffff
GogoFox <gogo-2001@o2.pl>
2020-11-11 00:36:37 +0100

    bugfix

--- commands/server.js
+++ commands/server.js
@@ -2,14 +2,14 @@ module.exports = {
     name: "server",
     description: "Shows server informations.",
     execute(message, args) {
-		message.channel.send(`Server name: ${message.guild.name}\n
-		Server names acronym: ${message.guild.nameAcronym}\n
-		Total members: ${message.guild.memberCount}\n
-		Server creation data: ${message.guild.createdAt}\n
-		Server region: ${message.guild.region}\n
-		Server owner: ${message.guild.owner}\n
-		Channel available: ${message.guild.channels}\n
-		Emojis: ${message.guild.emojis}\n
-		My name: ${message.guild.me}\n`);
+		message.channel.send(`Server name: ${message.guild.name}
+		Server names acronym: ${message.guild.nameAcronym}
+		Total members: ${message.guild.memberCount}
+		Server creation data: ${message.guild.createdAt}
+		Server region: ${message.guild.region}
+		Server owner: ${message.guild.owner}
+		Channel available: ${message.guild.channels}
+		Emojis: ${message.guild.emojis}
+		My name: ${message.guild.me}`);
   }
 };
