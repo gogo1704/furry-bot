87cdb557071f29ba3b97aa95f15e14e00acb7215
GogoFox <gogo-2001@o2.pl>
2020-11-11 03:30:19 +0100

    unstable up command

--- commands/up.js
+++ commands/up.js
@@ -3,14 +3,10 @@ module.exports = {
 	name: "up",
 	description: "Execute command above once again.",
 	execute(message, args) {
-		let filtered;
-		message.channel.messages.fetch({ limit: 40 }) 
-  			.then(messages => filtered = messages.filter(m => m.author.username === message.author.username && /^;.*/.test(m.content) && m.content !== `${prefix}up`).first())
+		message.channel.messages.fetch({ limit: 30 }) 
+  			.then(messages => message.client.emit("message", messages.filter(m => m.author.username === message.author.username && /^;.*/.test(m.content) && m.content !== `${prefix}up`).first()))
   			.catch(console.error);
-  		if (typeof filtered !== "undefined") {
-  			message.client.emit("message", filtered);
-  		}
+
+  		
 	}
 };
-// message.client.emit("message", .first())
-// messages.filter(m => m.author.username === message.author.username && /^;.*/.test(m.content) && m.content !== `${prefix}up`)
