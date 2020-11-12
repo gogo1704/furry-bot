b4eabb1f15672ada85adf674d477ab0f6b276a64
KrabboStabbo <44028224+BrzesiekGrydniak@users.noreply.github.com>
2020-11-12 20:47:23 +0100

    Update shoot.js

--- commands/shoot.js
+++ commands/shoot.js
@@ -1,3 +1,5 @@
+let log = require("../local/hp.json");
+const fs = require("fs");
 module.exports = {
     name: "shoot",
     description: "Shoot a targeted user.",
@@ -6,10 +8,21 @@ module.exports = {
     execute(message, args) {
         const user = message.mentions.users.first();
         if (user) {
-            const member = message.guild.member(user);
-                if (member) {
-                        message.channel.send(`You've shot ${user}!`);
-                }
+            if (message.guild.member(user)) {
+				message.channel.send(`You've shot ${user}`);
+				if(log[user.id]){
+					log[user.id]--;
+					// w razie czego zapierdolcie to stąd 
+				} else {
+					log[user.id] = 99;	
+				}
+			message.channel.send(`and he has ${log[user.id]}/100HP!`);
+				try {
+					fs.writeFileSync("./local/hp.json", JSON.stringify(log));
+				} catch(error) {
+					console.error(error);
+				}
+            }
         } else {
           message.channel.send("Who do you want to kill?");
         }
