7937ec8346af0347b4f0dd2fbd0b4c83b4f280c4
GogoFox <gogo-2001@o2.pl>
2020-11-15 21:51:01 +0100

    ( ͡° ͜ʖ ͡°)

---
+++ commands/lenny.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: "lenny",
+	description: "( ͡° ͜ʖ ͡°)",
+    allowPM: true,
+	execute(message, args) {
+		message.channel.send("( ͡° ͜ʖ ͡°)");
+		// jprd ale to creepy wygląda z mają czcionką
+	}
+};
