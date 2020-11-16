2617a7fc25473b6fe57a6da387c9fa5409785922
Lüfv <43139327+lufv@users.noreply.github.com>
2020-11-16 01:21:50 +0100

    Update flip.js

--- commands/flip.js
+++ commands/flip.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: "flip",
 	description: "Flips the coin.",
-    allowPM: true,
+    	allowPM: true,
 	execute(message, args) {
 		const side = Math.floor(Math.random() * 2)===0 ? "heads" : "tails";
 		message.channel.send(`You flipped a coin. It landed on ${side}!`);
