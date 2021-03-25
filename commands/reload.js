3360aad55726e546c84e3dda6ca595623f18c1b5
Lüfv <sugarsop_reflexional@aleeas.com>
2021-03-25 10:34:36 +0100

    reload.js: reload modules/*.js too

--- commands/reload.js
+++ commands/reload.js
@@ -4,12 +4,18 @@ module.exports = {
 	description: 'Reloads all commands.',
 	cooldown: 5,
 	execute(message, args) {
-		console.log("Reloading commands...");
+		console.log("Reloading commands and modules...");
 		const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+    const moduleFiles = fs.readdirSync("./modules").filter(file => file.endsWith(".js"));
+
 		for (const file of commandFiles) {
 			delete require.cache[require.resolve(`./${file}`)];
 			const command = require(`../commands/${file}`);
 			message.client.commands.set(command.name, command);
+    }
+
+    for (const file of moduleFiles) {
+			delete require.cache[require.resolve(`../modules/${file}`)];
 		}
 	}
 };
