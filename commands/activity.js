09268da7074e5f99436eb53e2cb8035916adb5d4
Lüfv <sugarsop_reflexional@aleeas.com>
2021-03-25 10:51:20 +0100

    activity.js randomise status

--- commands/activity.js
+++ commands/activity.js
@@ -1,15 +1,80 @@
-module.exports = {
+const fs = require("fs");
+const filecheck = require("../modules/filecheck.js");
 
+module.exports = {
 	name: "activity",
 	description: "Sets bot activity!",
 	args: true,
 	usage: `playing | streaming | listening | watching | competing <activity>`,
-    	allowPM: true,
-    	cooldown: 10,
+  allowPM: true,
+  cooldown: 0,
 	execute(message, args) {
 
-		message.client.user.setActivity("");			// This sometimes helps if activity bugs or lags. It deletes activity on restarting bot, but not on reload
-		message.client.user.setActivity(args.slice(1).join(" "), { type: args[0].toUpperCase() });
-		
-	}
-};
+		let dictionary = {
+							"verbs": ["playing", "listening", "watching", "competing"],
+							"adjectives": [],
+							"nouns": []
+						 };
+
+		const path = "./modules/dictionary.json";
+		filecheck.check(path, JSON.stringify(dictionary));	// 1st arg: path to file, 2nd arg: default string, if file not found
+  	const file = fs.readFileSync(path);
+		dictionary = JSON.parse(file);
+
+		switch (args[0].toString().toLowerCase()) {
+			case "clear":
+				message.client.user.setActivity("");
+				break;
+
+			case "set":
+				if (subarg) {
+					message.client.user.setActivity("");	// this sometimes helps if activity bugs or lags
+					message.client.user.setActivity(args.slice(2).join(" "), { type: args[1].toUpperCase() });
+				} else {
+					message.channel.send("Usage: `'activity set <playing | listening | watching | competing> <activity>`");
+				}
+				break;
+
+			case "list":
+				message.channel.send("This freaking dictionary has 3477 entries! Do you really want me to print it all on this chat?");
+			  break;
+
+			case "add":
+				switch (subarg) {
+					case "adjective":
+						dictionary.adjectives.push(args.splice(2).join(" "));
+						break;
+					case "noun":
+						dictionary.nouns.push(args.splice(2).join(" "));
+						break;
+					default:
+						message.channel.send("Usage: `'activity add <adjective | noun>`");
+						break;
+				}
+				break;
+
+			case "random":
+				let verb = dictionary.verbs[Math.floor(Math.random() * dictionary.verbs.length)];
+				let adjective = dictionary.adjectives[Math.floor(Math.random() * dictionary.adjectives.length)];
+				let noun = dictionary.nouns[Math.floor(Math.random() * dictionary.nouns.length)];
+
+				let article = "";
+				let vowels = ["a", "e", "i", "o", "u", "y"];
+				if (vowels.indexOf(adjective.charAt(0)) !== -1) {
+					article = "an";
+				} else {
+					article = "a";
+				}
+
+				let status = article + " " + adjective + " " + noun;
+
+				message.client.user.setActivity("");	// this sometimes helps if activity bugs or lags
+				message.client.user.setActivity(status, { type: verb.toUpperCase() });
+				break;
+
+			default:
+				message.channel.send("Usage: `'activity set <playing | listening | watching | competing> <activity>`");
+		};
+	};
+}
+
