b1719fa6a352f94bc19e431ae2894aab7c377543
GogoFox <gogo-2001@o2.pl>
2021-10-22 20:40:03 +0200

    reverse.js mega regex fix

--- commands/reverse.js
+++ commands/reverse.js
@@ -5,8 +5,25 @@ module.exports = {
 	args: true,
 	usage: "<text>",
 	execute(message, args) {
+		let joinedArgs = args.join(" ");
+		console.log(joinedArgs);
+		let mentionRegex = /<@!?(\d+)>/g;
+		let mentionIterator = joinedArgs.matchAll(mentionRegex);
+		for (match of mentionIterator) {
+			joinedArgs = joinedArgs.replace(match[0], message.client.users.cache.get(match[1]).username);
+		}
+		let emojiRegex = /<:(\D+):(\d+)>/g;
+		let emojiIterator = joinedArgs.matchAll(emojiRegex);
+		for (match of emojiIterator) {
+			joinedArgs = joinedArgs.replace(match[0], match[0].split("").reverse().join(""));
+		}
+		let emojiUnicodeRegex = /\p{Emoji_Presentation}/gu;
+		let emojiUnicodeIterator = joinedArgs.matchAll(emojiUnicodeRegex);
+		for (match of emojiUnicodeIterator) {
+			joinedArgs = joinedArgs.replace(match[0], match[0].split("").reverse().join(""));
+		}
 
-        const answer = args.join(" ").split("").reverse().join("");
+        const answer = joinedArgs.split("").reverse().join("");
         message.channel.send(answer);
 
     }
